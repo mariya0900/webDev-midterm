32a168d2e1c0a917fddec0113aecfef25299006b
mariya0900 <maria.anashkina@ontariotechu.net>
2022-03-26 18:59:35 -0400

    commit

--- midterm/src/App.vue
+++ midterm/src/App.vue
@@ -1,16 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img src="./assets/Todo.png" class="logo" />
+  <ul :class="linksClass">
+    <li :class="linkClass" v-for="page in pages" :key="page">
+    <a href="#" v-on:click.prevent="this.currentPage=page.text">{{page.text}}</a></li>
+  </ul>
+  <MainPage />
+  <DisplayPage v-if="this.currentPage=='Home'"/>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainPage from './components/MainPage.vue'
+import DisplayPage from './components/display.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    MainPage,
+    DisplayPage,
+  },
+  data(){
+    return{
+      linkClass: 'link',
+      linksClass: 'links',
+      pages: [
+        {text: 'Home'},
+        {text: '|', isLink: false},
+        {text: 'Tasks'},
+        {text: '|', isLink: false},
+        {text: 'Logout'}
+      ],
+      currentPage: 'Home'
+    }
+  },
 }
 </script>
 
@@ -19,8 +42,35 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  background: #18a1db;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: auto;
+  margin-bottom: auto;
+  height: 26vh;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    
+  }
+}
+
+.logo{
+  height: 15vh;
+  width: 25vh;
+}
+
+.links{
+  overflow: hidden;
+}
+.link{
+  display: inline-block;
+  padding: 15px;
 }
 </style>
---
+++ midterm/src/components/display.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="home">
+    <table id="class">
+        <tr>
+        <td align="left"> Your name is: </td><td class="name"></td>
+        </tr>
+    </table>
+    <table class="contact">
+        <tr>
+            <td align="left">Email: </td><td class="email"></td>
+        </tr>
+    </table>
+    <table class="contact">
+        <tr>
+            <td align="left">Email: </td><td class="email"></td>
+        </tr>
+    </table>
+    <table class="contact">
+        <tr>
+            <td align="left">Email: </td><td class="email"></td>
+        </tr>
+    </table>
+    <table class="contact">
+        <tr>
+            <td align="left">Email: </td><td class="email"></td>
+        </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DisplayPage',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
